dbec1ffe1000a79500411aa628479e03e845b33c
Lautaro <lautaro@kubriksoftware.com>
2025-09-18 18:22:54 -0300

    First commit

---
+++ src/view/UsuarioView.java
@@ -0,0 +1,19 @@
+package view;
+import javax.swing.*;
+
+public class UsuarioView extends JFrame{
+    private JLabel labelID;
+    private JTextField textFieldID;
+    private JLabel labelName;
+    private JTextField textFieldName;
+    private JTextField textFieldEmail;
+    private JPasswordField passwordField;
+    private JTextField textFieldDate;
+    private JList listHabitos;
+    private JButton btnGuardar;
+    private JButton btnCancelar;
+    private JLabel labelEmail;
+    private JLabel LabelPass;
+    private JLabel LabelDate;
+    private JLabel LabelHabitos;
+}
